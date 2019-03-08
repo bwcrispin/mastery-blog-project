03a376395314d0c3b95fc4813ecb1ebdbc765a05
Ben Crispin <benjamin.crispin@gmail.com>
2019-03-08 13:36:21 -0500

    adding spring project items

---
+++ src/main/java/org/wecancodeit/masteryblogproject/MasteryBlogProjectApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.masteryblogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteryBlogProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteryBlogProjectApplication.class, args);
+	}
+
+}
